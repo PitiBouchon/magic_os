2cd90277af25e310002fb18bd7728c2de0c6e200
Pierre POLLET <pierre.pollet54@gmail.com>
2023-04-04 20:26:52 +0200

    Finally the fdt is working

--- src/dtb.rs
+++ src/dtb.rs
@@ -6,9 +6,7 @@ pub unsafe fn init_dtb(dtb: usize) {
     sbi_print_str("DTB at : ");
     sbi_println_number_base10(dtb);
 
-    match fdt::Fdt::from_ptr(dtb as *const u8) {
-        Ok(_) => sbi_println_str("OK"),
-        Err(fdt::FdtError::BadPtr) => sbi_println_str("BadPtr"),
-        _ => sbi_println_str("unknown error"),
-    }
+    let fdt = fdt::Fdt::from_ptr(dtb as *const u8).unwrap();
+    sbi_print_str("CPUs : ");
+    sbi_println_number_base10(fdt.cpus().count());
 }
