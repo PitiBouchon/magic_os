a703c7a035a9bba244d0cfac0af74ff4e6362195
Pitibouchon <pierre.pollet54@gmail.com>
2023-04-19 00:31:11 +0200

    Minor changes to test smth

--- src/allocator.rs
+++ src/allocator.rs
@@ -50,30 +50,66 @@ impl MyGlobalAllocator {
         }
         alloc.nodes = Some(node);
     }
+
+    unsafe fn print(mut nodes: &mut Option<NonNull<FreeMemoryNode>>, s: &str) {
+        println!("{s}");
+        while let Some(node) = nodes {
+            println!("Node({:p} | 0x{:x})", node.as_ptr(), node.as_ref().size);
+            nodes = &mut node.as_mut().next;
+            println!("--");
+        }
+    }
+}
+
+pub const fn align_round_down(addr: usize, align: usize) -> usize {
+    addr & !(align - 1)
+}
+
+pub const fn align_round_up(addr: usize, align: usize) -> usize {
+    align_round_down(addr + align - 1, align)
 }
 
 unsafe impl GlobalAlloc for MyGlobalAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         // TODO : respect alignment
-        let alloc = self.0.lock();
+        assert_eq!(layout.align(), 1);
+        let mut alloc = self.0.lock();
         let mut nodes = alloc.nodes;
+        Self::print(&mut nodes, "[alloc] start");
         let mut last_node_opt: NonNull<FreeMemoryNode> = alloc.nodes.unwrap(); // Should panic if no regions
         while let Some(mut non_null_node) = nodes {
             let node = non_null_node;
 
             if node.as_ref().size >= layout.size() {
-                if layout.size() + core::mem::size_of::<FreeMemoryNode>() > node.as_ref().size {
-                    let mut new_node =
-                        NonNull::new(last_node_opt.as_ptr().byte_offset(layout.size() as isize))
-                            .unwrap();
-                    *new_node.as_mut() = FreeMemoryNode {
-                        next: node.as_ref().next,
-                        size: node.as_ref().size - layout.size(),
-                    };
-                    last_node_opt.as_mut().next = Some(new_node);
-                } else {
+                if layout.size() + core::mem::size_of::<FreeMemoryNode>() < node.as_ref().size {
+                    let ptr_new_node = last_node_opt.as_ptr().byte_add(layout.size());
+                    // println!("A1: {:p}", ptr_new_node);
+                    // println!("A2: {}", ptr_new_node.align_offset(core::mem::align_of::<FreeMemoryNode>()));
+                    // ptr_new_node = ptr_new_node.add(ptr_new_node.align_offset(core::mem::align_of::<FreeMemoryNode>()));
+                    // println!("B1: {:p}", ptr_new_node);
+                    // println!("B2: {}", core::usize::MAX);
+                    // println!("B3: {}", core::mem::align_of::<FreeMemoryNode>());
+                    // assert!(ptr_new_node.is_aligned());
+                    // ptr_new_node = align_round_up(ptr_new_node as usize, core::mem::align_of::<FreeMemoryNode>()) as *mut FreeMemoryNode;
+                    core::ptr::write_unaligned(
+                        ptr_new_node,
+                        FreeMemoryNode {
+                            next: node.as_ref().next,
+                            size: node.as_ref().size - layout.size(),
+                        },
+                    );
+                    let new_node = NonNull::new(ptr_new_node).unwrap();
+                    if !alloc.nodes.contains(&last_node_opt) {
+                        last_node_opt.as_mut().next = Some(new_node);
+                    } else {
+                        alloc.nodes = Some(new_node);
+                    }
+                } else if !alloc.nodes.contains(&last_node_opt) {
                     last_node_opt.as_mut().next = node.as_ref().next;
+                } else {
+                    alloc.nodes = node.as_ref().next;
                 }
+                Self::print(&mut alloc.nodes, "[alloc] end");
                 return usize::from(node.addr()) as *mut u8;
             }
             last_node_opt = node;
@@ -83,8 +119,46 @@ unsafe impl GlobalAlloc for MyGlobalAllocator {
         panic!("Not enough space on the heap");
     }
 
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        todo!()
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        // Todo respect alignment
+        assert_eq!(layout.align(), 1);
+        println!("ptr: {:p}", ptr);
+        println!("Size: {}", layout.size());
+        println!("SizeOf<FreeMemoryNode>: {}", core::mem::size_of::<FreeMemoryNode>());
+        let mut alloc = self.0.lock();
+        let mut nodes = alloc.nodes;
+        Self::print(&mut nodes, "[alloc] start");
+        let mut last_node_opt: NonNull<FreeMemoryNode> = alloc.nodes.unwrap();
+        while let Some(mut non_null_node) = nodes {
+            if ptr.byte_add(layout.size()) == non_null_node.as_ptr().cast() {
+                let merged_size = non_null_node.as_mut().size + layout.size();
+                ptr.cast::<FreeMemoryNode>().write(FreeMemoryNode { next: non_null_node.as_ref().next, size: merged_size });
+                last_node_opt.as_mut().next = Some(NonNull::new(ptr.cast()).unwrap());
+                println!("Merged A");
+                Self::print(&mut alloc.nodes, "[alloc] start");
+                return
+            }
+            if non_null_node.as_ptr().byte_add(non_null_node.as_ref().size).cast() == ptr {
+                non_null_node.as_mut().size += layout.size();
+                println!("Merged B");
+                return
+            }
+
+            nodes = non_null_node.as_mut().next;
+        }
+        if layout.size() >= core::mem::size_of::<FreeMemoryNode>() {
+            core::ptr::write(
+                ptr.cast(),
+                FreeMemoryNode {
+                    next: alloc.nodes,
+                    size: layout.size(),
+                },
+            );
+            let new_node = NonNull::new(ptr.cast()).unwrap();
+            alloc.nodes = Some(new_node);
+            return
+        }
+        panic!("")
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@
 #![feature(slice_ptr_get)]
 #![feature(strict_provenance)]
 #![feature(pointer_byte_offsets)]
+#![feature(option_result_contains)]
+#![feature(pointer_is_aligned)]
 #![no_std]
 #![no_main]
 #![allow(unused_imports)]
@@ -25,7 +27,7 @@ use crate::vm::KERNEL_PAGE_TABLE;
 use core::panic::PanicInfo;
 use riscv::register::stvec::TrapMode;
 
-const OS_STACK_SIZE: usize = 65536;
+const OS_STACK_SIZE: usize = 65536; // Must be the same as in entry.S
 
 #[repr(C, align(16))]
 struct Stack([u8; OS_STACK_SIZE]);
@@ -38,7 +40,7 @@ extern "C" {
 }
 
 #[no_mangle]
-fn main(hart_id: usize, dtb: usize) -> ! {
+fn main(_hart_id: usize, dtb: usize) -> ! {
     println!("---------- Kernel Start ----------");
 
     println!("> Setup kernel trap");
@@ -57,7 +59,7 @@ fn main(hart_id: usize, dtb: usize) -> ! {
     vm::init_paging(&fdt);
     allocator::init_heap();
 
-    let test = alloc::string::String::from("Hell World !");
+    let test = alloc::string::String::from("Hello World !");
     println!("{}", test);
     drop(test);
 
